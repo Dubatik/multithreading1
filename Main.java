45e11a6b01ea32f0e2c9726382c5e54561be210b
Dubatik <dubatik@icloud.com>
2023-07-08 22:43:00 +0300

    new

--- Main.java
+++ Main.java
@@ -1,37 +1,16 @@
 import java.util.*;
 
 public class Main {
+    private static String text;
 
     public static void main(String[] args) throws InterruptedException {
 
         Runnable logic = () -> {
-
-            String text = generateText("aab", 30_000);
-            int maxSize = 0;
-            for (int i = 0; i < text.length(); i++) {
-                for (int j = 0; j < text.length(); j++) {
-                    if (i >= j) {
-                        continue;
-                    }
-                    boolean bFound = false;
-                    for (int k = i; k < j; k++) {
-                        if (text.charAt(k) == 'b') {
-                            bFound = true;
-                            break;
-                        }
-                    }
-                    if (!bFound && maxSize < j - i) {
-                        maxSize = j - i;
-                    }
-                }
-            }
-            System.out.println(text.substring(0, 100) + " -> " + maxSize);
+            lineOfLetter();
         };
-        long startTs = System.currentTimeMillis(); // start time
 
         List<Thread> threads = new ArrayList<>();
-
-        for (int i = 0; i < 25; i++) {
+        for (int i = 0; i < 1000; i++) {
             threads.add(new Thread(logic));
             threads.get(i).start();
         }
@@ -40,17 +19,46 @@ public class Main {
             thread.join();
         }
 
-        long endTs = System.currentTimeMillis(); // end time
-        System.out.println("Time: " + (endTs - startTs) + "ms");
-
+        Set<Map.Entry<Integer, Integer>> entries = sizeToFreq.entrySet();
+        Integer keyBig = 0;
+        for (Map.Entry<Integer, Integer> pair : entries) {
+            Integer key = pair.getKey();
+            if (key > keyBig) {
+                keyBig = key;
+            }
+        }
+        System.out.println("Самое частое количество повторений " + keyBig
+                + " (встретилось " + sizeToFreq.get(keyBig) + " раз)");
+        System.out.println("Другие размеры:");
+        for (Map.Entry<Integer, Integer> pair : entries) {
+            Integer key = pair.getKey();
+            Integer value = pair.getValue();
+            System.out.println("- " + key + " (" + value + " раз) ");
+        }
     }
 
-    public static String generateText(String letters, int length) {
-        Random random = new Random();
-        StringBuilder text = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            text.append(letters.charAt(random.nextInt(letters.length())));
+    public static synchronized void lineOfLetter() {
+        text = generateRoute("RLRFR", 100);
+        int maxSize = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == 'R') maxSize++;
         }
-        return text.toString();
+        if (!sizeToFreq.containsKey(maxSize)) {
+            sizeToFreq.put(maxSize, 1);
+        } else {
+            int newNum = sizeToFreq.get(maxSize) + 1;
+            sizeToFreq.put(maxSize, newNum);
+        }
+    }
+
+    public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+
+    public static String generateRoute(String letters, int length) {
+        Random random = new Random();
+        StringBuilder route = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            route.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return route.toString();
     }
 }
